2b54233efa6fb904bc90bba634e41b3e2772f83d
GeekZeke <zekeellis1@gmail.com>
2026-01-11 18:38:30 -0500

    reset for new year

---
+++ ponder/variables-constants.js
@@ -0,0 +1,25 @@
+const PI = 3.14;
+let radius = 3
+
+let area = radius * radius * PI;
+console.log(area);
+
+radius = 20;
+
+console.log(area);
+
+const one = 1;
+const two = '2';
+
+let result = one * two;
+console.log(result);
+
+//Scope
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);
+    console.log(student);
+}
+console.log(course);
+console.log(student;)
